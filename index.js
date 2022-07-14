7b3e68a95898cb7047a99d39b75e3ad455e46075
Glorious <gloriouselijah125@gmail.com>
2022-07-14 20:45:49 +0000

    web crawler that logs links to console

---
+++ index.js
@@ -0,0 +1,59 @@
+import fetch from "node-fetch";
+import cheerio from "cheerio";
+
+//stores link
+let webLink = "http://";
+
+//stores argument that sets workers
+let workers;
+
+//tracks links that has been visited
+let seen = {};
+
+//goes through argument to find arguments that match a link and contains a number and assigns them respectively
+if (process.argv.length) {
+	process.argv.map((arg, i) => {
+		if (arg.includes("www")) {
+			webLink = webLink + arg.slice(1, arg.length - 1);
+		}
+		if (!isNaN(arg.slice(1, arg.length - 1))) {
+			workers = arg.slice(1, arg.length - 1);
+		}
+	});
+}
+
+// web crawler function
+const crawler = async (url) => {
+	const base = url;
+	try {
+		console.log("Crawling:", url);
+		if (seen.url) {
+			return;
+		}
+		if (url.includes(String(base))) {
+			let res = await fetch(url);
+			seen[url] = true;
+			const html = await res.text();
+			const $ = cheerio.load(html);
+			const links = $("a")
+				.map((i, link) => link.attribs.href)
+				.get();
+
+			links.map((link, i) => {
+				crawler(link);
+			});
+		}
+	} catch {
+		console.log("Invalid Url");
+	}
+};
+
+//track crawler count
+let count = 1;
+
+// a loop to create multiple crawlers bassed on input
+while (workers >= 1) {
+	crawler(webLink);
+	count;
+	workers -= 1;
+}
